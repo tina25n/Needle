7ed85f4dde075f2372f59b370d8bbe88f4314cd9
cheesebricked <131835612+cheesebricked@users.noreply.github.com>
2024-03-09 15:03:56 -0800

    added window, and const variables

---
+++ const.py
@@ -0,0 +1,9 @@
+width = 1280
+height = 720
+
+# IBM COLOR BLIND SAFE PALETTE
+red = (220, 38, 127)
+purple = (120, 94, 240)
+blue = (100, 143, 255)
+orange = (254, 97, 0)
+yellow = (255, 176, 0)
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import pygame
+from const import *
+
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Sixth Sense')
+screen.fill(red)
+pygame.display.flip()
+
+running = True
+
+
+# game loop 
+while running: 
+    
+# for loop through the event queue   
+    for event in pygame.event.get(): 
+      
+        # Check for QUIT event       
+        if event.type == pygame.QUIT: 
+            running = False
