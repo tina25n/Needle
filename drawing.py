bdad32257674f0511a32e7032d5ebf6b4f8a7088
cheesebricked <131835612+cheesebricked@users.noreply.github.com>
2024-03-09 15:50:14 -0800

    added button, added StartMenu class in drawing, added skeleton of state machine(???)

--- drawing.py
+++ drawing.py
@@ -1,1 +1,14 @@
 import pygame
+from const import *
+
+class StartMenu():
+    def __init__(self):
+        pass
+
+    def drawPlayButton(self):
+        rectWidth = width / 3
+        rectHeight = height / 7
+        pygame.draw.rect(screen, red, pygame.Rect((width / 2) - rectWidth /2, ((height / 5) * 3) - rectHeight/2, rectWidth, rectHeight),  0, 3)
+
+    def draw(self):
+        self.drawPlayButton()
